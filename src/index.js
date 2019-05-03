c88fa633ac0ecaeb5e60b6c37cb6cc8b7340450b
Paul Petrov <paul@bargamut.ru>
2019-05-03 13:43:01 +0300

    Установить React и React DOM и создать компонент

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MainPage from './components/main/main.jsx';
 
+const App = () => {
+  return <MainPage />;
+};
+
+const init = () => {
+  ReactDOM.render(
+      <App />,
+      document.querySelector(`#root`)
+  );
+};
+
+init();
