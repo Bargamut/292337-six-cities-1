b57fb646a29c560512fcb451c63f4dc41072e798
Paul Petrov <paul@bargamut.ru>
2019-05-05 23:49:43 +0300

    Вынести компонент App в components/

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MainPage from './components/main/main.jsx';
-
-const App = () => {
-  return <MainPage />;
-};
+import App from './components/app/app.jsx';
 
 const init = () => {
   ReactDOM.render(
