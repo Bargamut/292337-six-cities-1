2affc55631689fbecdeea299e8113f07f8ec979f
Paul Petrov <paul@bargamut.ru>
2019-05-06 01:20:24 +0300

    Валидировать props, передать данные недвижимости
    * Реализовать генерацию компонентов на основе
      массива даных

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MainPage from '../main/main.jsx';
 
-const App = () => {
-  return <MainPage />;
+const App = (props) => {
+  const {citiesPlaces: placesNames} = props;
+
+  return <MainPage
+    placesNames={placesNames}
+  />;
 };
+
+App.propTypes = {
+  citiesPlaces: PropTypes.arrayOf(PropTypes.string)
+};
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app.jsx';
 
+const citiesPlaces = [
+  `Beautiful &amp; luxurious apartment at great location`,
+  `Wood and stone place`,
+  `Canal View Prinsengracht`,
+  `Nice, cozy, warm big bed apartment`
+];
+
 const init = () => {
   ReactDOM.render(
-      <App />,
+      <App
+        citiesPlaces={citiesPlaces}
+      />,
       document.querySelector(`#root`)
   );
 };
