2463191346cf99cd923b8d334ee894bbf720723e
Paul Petrov <paul@bargamut.ru>
2019-05-13 15:33:26 +0300

    Исправить структуру Mock-данных, создать настройки

--- src/mocks/offers.js
+++ src/mocks/offers.js
@@ -6,34 +6,29 @@ const offers = [
     name: `Beautiful &amp; luxurious apartment at great location`,
     price: {
       value: 120,
-      currency: `&euro;`,
-      text: `&#47;&nbsp;night`
+      currency: `€`
     },
-    rating: `93%`
+    rating: 93
   },
   {
     type: `Private room`,
     img: `img/room.jpg`,
-    mark: null,
     name: `Wood and stone place`,
     price: {
       value: 80,
-      currency: `&euro;`,
-      text: `&#47;&nbsp;night`
+      currency: `€`
     },
-    rating: `80%`
+    rating: 80
   },
   {
     type: `Apartment`,
     img: `img/apartment-02.jpg`,
-    mark: null,
     name: `Canal View Prinsengracht`,
     price: {
       value: 132,
-      currency: `&euro;`,
-      text: `&#47;&nbsp;night`
+      currency: `€`
     },
-    rating: `80%`
+    rating: 80
   },
   {
     type: `Apartment`,
@@ -42,10 +37,9 @@ const offers = [
     name: `Nice, cozy, warm big bed apartment`,
     price: {
       value: 180,
-      currency: `&euro;`,
-      text: `&#47;&nbsp;night`
+      currency: `€`
     },
-    rating: `100%`
+    rating: 100
   }
 ];
 
