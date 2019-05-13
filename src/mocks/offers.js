f8550ba103c3a6bf912a982082f4436fb1bc2539
Paul Petrov <paul@bargamut.ru>
2019-05-13 12:51:23 +0300

    Создать src/mocks/offers.js и структуру карточек

---
+++ src/mocks/offers.js
@@ -0,0 +1,48 @@
+const offers = [
+  {
+    type: `Private room`,
+    name: `Beautiful &amp; luxurious apartment at great location`,
+    img: `img/room.jpg`,
+    price: {
+      value: 80,
+      currency: `&euro;`,
+      text: `&#47;&nbsp;night`
+    },
+    rating: `80%`
+  },
+  {
+    type: `Private room`,
+    name: `Wood and stone place`,
+    img: `img/room.jpg`,
+    price: {
+      value: 80,
+      currency: `&euro;`,
+      text: `&#47;&nbsp;night`
+    },
+    rating: `80%`
+  },
+  {
+    type: `Private room`,
+    name: `Canal View Prinsengracht`,
+    img: `img/room.jpg`,
+    price: {
+      value: 80,
+      currency: `&euro;`,
+      text: `&#47;&nbsp;night`
+    },
+    rating: `80%`
+  },
+  {
+    type: `Private room`,
+    name: `Nice, cozy, warm big bed apartment`,
+    img: `img/room.jpg`,
+    price: {
+      value: 80,
+      currency: `&euro;`,
+      text: `&#47;&nbsp;night`
+    },
+    rating: `80%`
+  }
+];
+
+export default offers;
