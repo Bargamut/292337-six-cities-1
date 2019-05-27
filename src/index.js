0db27d9cbb215fc0fed9b9422b99941c7e47e66e
Paul Petrov <paul@bargamut.ru>
2019-05-27 04:14:00 +0300

    Создать хранилище приложения

--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app.jsx';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 
 import settings from './mocks/settings';
 import offers from './mocks/offers';
+import {reducer} from '../reducer/reducer.js';
+
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const init = (citiesPlaces, appSettings) => {
   ReactDOM.render(
-      <App
-        cityCoords={appSettings.cityCoords}
-        citiesPlaces={citiesPlaces}
-        onClickCardHeader={appSettings.onClickCardHeader}
-        onClickCardImage={appSettings.onClickCardImage}
-      />,
+      <Provider store={store}>
+        <App
+          cityCoords={appSettings.cityCoords}
+          citiesPlaces={citiesPlaces}
+          onClickCardHeader={appSettings.onClickCardHeader}
+          onClickCardImage={appSettings.onClickCardImage}
+        />
+      </Provider>,
       document.querySelector(`#root`)
   );
 };
