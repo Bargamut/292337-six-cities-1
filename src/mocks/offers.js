815aa2b1251e62ff006896189df973f73a61a378
Paul Petrov <paul@bargamut.ru>
2019-05-27 23:34:29 +0300

    Добавить больше offer'ов

--- src/mocks/offers.js
+++ src/mocks/offers.js
@@ -1,54 +1,225 @@
 const offers = [
   {
-    type: `Apartment`,
-    img: `img/apartment-01.jpg`,
-    mark: `Premium`,
-    name: `Beautiful &amp; luxurious apartment at great location`,
+    city: `Amsterdam`,
+    coords: [52.3909553943508, 4.85309666406198],
+    img: `apartment-01.jpg`,
+    isPremium: true,
     price: {
       value: 120,
       currency: `€`
     },
     rating: 93,
-    coords: [52.3909553943508, 4.85309666406198],
-    city: `Amsterdam`
+    title: `Beautiful & luxurious apartment at great location`,
+    type: `apartment`
   },
   {
-    type: `Private room`,
-    img: `img/room.jpg`,
-    name: `Wood and stone place`,
+    city: `Amsterdam`,
+    coords: [52.369553943508, 4.85309666406198],
+    img: `room.jpg`,
+    isPremium: false,
     price: {
-      value: 80,
+      value: 120,
       currency: `€`
     },
     rating: 80,
-    coords: [52.369553943508, 4.85309666406198],
-    city: `Amsterdam`
+    title: `Wood and stone place`,
+    type: `room`
   },
   {
-    type: `Apartment`,
-    img: `img/apartment-02.jpg`,
-    name: `Canal View Prinsengracht`,
+    city: `Amsterdam`,
+    coords: [52.3909553943508, 4.929309666406198],
+    img: `apartment-02.jpg`,
+    isPremium: false,
     price: {
       value: 132,
       currency: `€`
     },
     rating: 80,
-    coords: [52.3909553943508, 4.929309666406198],
-    city: `Amsterdam`
+    title: `Canal View Prinsengracht`,
+    type: `apartment`
   },
   {
-    type: `Apartment`,
-    img: `img/apartment-03.jpg`,
-    mark: `Premium`,
-    name: `Nice, cozy, warm big bed apartment`,
+    city: `Amsterdam`,
+    coords: [52.3809553943508, 4.939309666406198],
+    img: `apartment-03.jpg`,
+    isPremium: true,
     price: {
       value: 180,
       currency: `€`
     },
     rating: 100,
-    coords: [52.3809553943508, 4.939309666406198],
-    city: `Amsterdam`
-  }
+    title: `Nice, cozy, warm big bed apartment`,
+    type: `apartment`
+  },
+  {
+    city: `Amsterdam`,
+    coords: [52.3808573943508, 4.974609666406198],
+    img: `apartment-03.jpg`,
+    isPremium: true,
+    price: {
+      value: 180,
+      currency: `€`
+    },
+    rating: 100,
+    title: `Nice, cozy, warm big bed apartment`,
+    type: `apartment`
+  },
+  {
+    city: `Brussels`,
+    coords: [50.853585, 4.376354],
+    img: `apartment-03.jpg`,
+    isPremium: true,
+    price: {
+      value: 180,
+      currency: `€`
+    },
+    rating: 100,
+    title: `Nice, cozy, warm big bed apartment`,
+    type: `apartment`
+  },
+  {
+    city: `Brussels`,
+    coords: [50.845287, 4.316633],
+    img: `apartment-03.jpg`,
+    isPremium: true,
+    price: {
+      value: 180,
+      currency: `€`
+    },
+    rating: 100,
+    title: `Nice, cozy, warm big bed apartment`,
+    type: `apartment`
+  },
+  {
+    city: `Brussels`,
+    coords: [50.864956, 4.308865],
+    img: `apartment-03.jpg`,
+    isPremium: true,
+    price: {
+      value: 180,
+      currency: `€`
+    },
+    rating: 100,
+    title: `Nice, cozy, warm big bed apartment`,
+    type: `apartment`
+  },
+  {
+    city: `Brussels`,
+    coords: [50.829299, 4.349649],
+    img: `apartment-03.jpg`,
+    isPremium: true,
+    price: {
+      value: 180,
+      currency: `€`
+    },
+    rating: 100,
+    title: `Nice, cozy, warm big bed apartment`,
+    type: `apartment`
+  },
+  {
+    city: `Cologne`,
+    coords: [50.959209, 6.955255],
+    img: `apartment-03.jpg`,
+    isPremium: true,
+    price: {
+      value: 180,
+      currency: `€`
+    },
+    rating: 100,
+    title: `Nice, cozy, warm big bed apartment`,
+    type: `apartment`
+  },
+  {
+    city: `Cologne`,
+    coords: [50.933364, 6.999804],
+    img: `apartment-03.jpg`,
+    isPremium: true,
+    price: {
+      value: 180,
+      currency: `€`
+    },
+    rating: 100,
+    title: `Nice, cozy, warm big bed apartment`,
+    type: `apartment`
+  },
+  {
+    city: `Dusseldorf`,
+    coords: [51.260121, 6.802140],
+    img: `apartment-03.jpg`,
+    isPremium: true,
+    price: {
+      value: 180,
+      currency: `€`
+    },
+    rating: 100,
+    title: `Nice, cozy, warm big bed apartment`,
+    type: `apartment`
+  },
+  {
+    city: `Dusseldorf`,
+    coords: [51.228144, 6.849567],
+    img: `apartment-03.jpg`,
+    isPremium: true,
+    price: {
+      value: 180,
+      currency: `€`
+    },
+    rating: 100,
+    title: `Nice, cozy, warm big bed apartment`,
+    type: `apartment`
+  },
+  {
+    city: `Dusseldorf`,
+    coords: [51.200451, 6.821407],
+    img: `apartment-03.jpg`,
+    isPremium: true,
+    price: {
+      value: 180,
+      currency: `€`
+    },
+    rating: 100,
+    title: `Nice, cozy, warm big bed apartment`,
+    type: `apartment`
+  },
+  {
+    city: `Hamburg`,
+    coords: [53.581753, 10.074024],
+    img: `apartment-03.jpg`,
+    isPremium: true,
+    price: {
+      value: 180,
+      currency: `€`
+    },
+    rating: 100,
+    title: `Nice, cozy, warm big bed apartment`,
+    type: `apartment`
+  },
+  {
+    city: `Hamburg`,
+    coords: [53.553759, 9.923161],
+    img: `apartment-03.jpg`,
+    isPremium: true,
+    price: {
+      value: 180,
+      currency: `€`
+    },
+    rating: 100,
+    title: `Nice, cozy, warm big bed apartment`,
+    type: `apartment`
+  },
+  {
+    city: `Paris`,
+    coords: [48.868735, 2.349666],
+    img: `apartment-03.jpg`,
+    isPremium: true,
+    price: {
+      value: 180,
+      currency: `€`
+    },
+    rating: 100,
+    title: `Nice, cozy, warm big bed apartment`,
+    type: `apartment`
+  },
 ];
 
 export default offers;
