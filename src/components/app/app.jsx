7294edc7fa65924e325c1e59f245b1a61d5cfc79
Paul Petrov <paul@bargamut.ru>
2019-06-17 07:21:12 +0300

    Переработка App

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Header from '../../header/header.jsx';
-import SvgSprite from '../svg-sprite/svg-sprite.jsx';
-
 const App = (props) => {
   const {renderScreen} = props;
 
   return (
     <div>
-      <SvgSprite />
-
-      <Header />
-
       {renderScreen()}
     </div>
   );
