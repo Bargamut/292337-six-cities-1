6f10314d25b608d0e0e18c7d89bff4b2a5ebcaa1
Paul Petrov <paul@bargamut.ru>
2019-06-17 07:22:10 +0300

    Вынести компоненты SVG-спрайта и Header в index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app/app.jsx';
+
 import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
@@ -11,6 +11,13 @@ import reducer from './reducer/reducer';
 import {Operation} from './reducer/data/data';
 
 import withScreenSwitch from './hocs/with-screen-switch/with-screen-switch';
+import withUserNavigation from './hocs/with-user-navigation/with-user-navigation';
+
+import App from './components/app/app.jsx';
+import SvgSprite from './components/svg-sprite/svg-sprite.jsx';
+import Header from './components/header/header.jsx';
+
+const HeaderWrapped = withUserNavigation(Header);
 
 const AppWrapped = withScreenSwitch(App);
 
@@ -31,6 +38,10 @@ const init = () => {
 
   ReactDOM.render(
       <Provider store={store}>
+        <SvgSprite />
+
+        <HeaderWrapped />
+
         <AppWrapped />
       </Provider>,
       document.querySelector(`#root`)
