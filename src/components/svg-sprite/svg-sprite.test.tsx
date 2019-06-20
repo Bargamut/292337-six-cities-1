eba03929dbe2b2f1e9cad19e8aabed6bc026b802
Paul Petrov <paul@bargamut.ru>
2019-06-21 01:17:41 +0300

    Fix SvgSprite props validation & tests

--- src/components/svg-sprite/svg-sprite.test.tsx
+++ src/components/svg-sprite/svg-sprite.test.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as renderer from 'react-test-renderer';
 
-import SvgSprite from './svg-sprite.jsx';
+import SvgSprite from './svg-sprite';
 
 it(`SvgSprite correctly renders after relaunch`, () => {
   const svgSprite = renderer.create(<SvgSprite />).toJSON();
