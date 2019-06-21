582c9b37872f69f7ae1e27720b9ce9a822199c5b
Paul Petrov <paul@bargamut.ru>
2019-06-21 04:56:11 +0300

    Добавить проверку залогинен ли пользователь

--- src/reducer/user/selectors.js
+++ src/reducer/user/selectors.js
@@ -3,6 +3,8 @@ import {normalizeKeys} from '../../helpers';
 
 const NAME_SPACE = NameSpace.USER;
 
-export const checkAuthorization = (state) => state[NAME_SPACE].isAuthorizationRequired;
+export const checkAuthorizationRequired = (state) => state[NAME_SPACE].isAuthorizationRequired;
+
+export const checkAuthorization = (state) => (Object.keys(state[NAME_SPACE].user).length > 0);
 
 export const getUserInfo = (state) => normalizeKeys(state[NAME_SPACE].user);
